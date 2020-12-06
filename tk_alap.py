b12c27e534757608cf66ba7d9f467849105fe032
fsR3tR0 <fsR3tR0@github.com>
2020-12-06 16:43:40 -0600

    Uj verzio, plusz label van benne

---
+++ tk_alap.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+from tkinter import *
+
+root = Tk()
+root.title("tEsT")
+root.geometry('200x200')
+root.config(bg='green')
+
+l_test = Label(root, text="Helo belo")
+l_test.pack()
+
+l_uj = Label(root,text="Ez mar uj verzio")
+l_uj.pack()
+
+root.mainloop()

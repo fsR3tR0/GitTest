6d1c55ba6b270daac6ccab617bbf44f9c5a95092
fsR3tR0 <fsR3tR0@github.com>
2020-12-07 19:49:14 -0600

    Inccludok ebben lesznek/vannak

---
+++ inc.h
@@ -0,0 +1,13 @@
+#ifndef _INC_H_INCLUDED
+#define _INC_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef unsigned char uch;
+
+#endif
